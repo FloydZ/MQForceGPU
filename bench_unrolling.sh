0e03b0aacef5bea55013204e4cd7c1b0f3104ae1
Floyd Zweydinger <floyd.zweydinger@rub.de>
2023-01-22 13:18:32 +0100

    updated bash scripts

--- bench_unrolling.sh
+++ bench_unrolling.sh
@@ -4,11 +4,11 @@ K=20
 for n in {35..40}; do
 	# ab 13 wird die compilezeit unchristlich
 	for u in {7..10}; do
-		for w in {5..10}; do
+		for d in {1..3}; do
 			make clean  &> /dev/null
-			python3 gen_shared_kernel.py -u ${u} -w ${w} > kernel.inc  
+			python3 gen_shared_kernel.py -u ${u} -d ${d} > kernel.inc  
 			make UNROLL=${u} MAX_K=${K}  &> /dev/null
-			echo "n=${n}, K=${K} UNROLL=${u}, w=${w}"
+			echo "n=${n}, K=${K} UNROLL=${u}, d=${d}"
 			python3 gen_sys.py -m ${n} -n ${n} | ./guess 0
 			echo "\n\n"
 		done
